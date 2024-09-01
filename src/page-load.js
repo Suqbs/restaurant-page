b541d396762b3820abf84622b33ff958771d22a7
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-01 14:12:34 +0300

    add intro class to introduction p

--- src/page-load.js
+++ src/page-load.js
@@ -10,6 +10,7 @@ image.src = coffeeImage;
 image.alt = "coffee";
 
 const intro = document.createElement("p");
+intro.classList.add("intro");
 intro.textContent =
   `Discover a culinary journey like no other at FURKAN RESTAURANT. Our passion for authentic Turkish cuisine shines through in every dish, crafted with the finest ingredients and served with a touch of warmth and hospitality. Whether you're seeking a casual meal or a special occasion, our menu offers a diverse range of options to satisfy every palate. From succulent kebabs to flavorful stews, each bite is a testament to our commitment to culinary excellence. Join us for a memorable dining experience at FURKAN RESTAURANT, where the flavors of Turkey come alive.`;
 
