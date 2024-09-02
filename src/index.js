0c3f39ac9d5a9049725e9825a1379e166ceeb161
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-02 23:56:32 +0300

    Add menu-page logic but it is not complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { LoadHomePage } from "./home-page";
+import { LoadMenuPage } from "./menu-page";
 
 const content = document.getElementById("content");
 
 // LoadHomePage(content);
+LoadMenuPage(content);
---
+++ src/menu-page.js
@@ -0,0 +1,74 @@
+import kebabImage from "./image/kebab-plate.jpg";
+import lambShishImage from "./image/lamb-shish.jpg";
+import chickenAdanaImage from "./image/chicken-adana.jpg";
+
+// const card1 = document.createElement("div");
+
+// const cardTitle = document.createElement("h2");
+// cardTitle.classList.add("card-title");
+
+// const cardIntro = document.createElement("p");
+// cardIntro.classList.add("card-intro");
+
+// const image = document.createElement("img");
+// image.src = kebabImage;
+// image.alt = "kebab plate";
+
+function createCard(title, intro, imgSrc, imgAlt) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardTitle = document.createElement("h2");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = title;
+
+  const cardIntro = document.createElement("p");
+  cardIntro.classList.add("card-intro");
+  cardIntro.textContent = intro;
+
+  const image = document.createElement("img");
+  image.src = imgSrc;
+  image.alt = imgAlt;
+
+  card.appendChild(cardTitle);
+  card.appendChild(cardIntro);
+  card.appendChild(image);
+
+  cardList.push(card);
+}
+
+const cardList = [];
+
+export function LoadMenuPage(content) {
+  for (let i = 0; i < cardList.length; i++) {
+    content.appendChild(cardList[i]);
+
+    if (i !== cardList.length - 1) {
+      const roundHr = document.createElement("hr");
+      roundHr.classList.add("round-style");
+
+      content.appendChild(roundHr);
+    }
+  }
+}
+
+createCard(
+  "Kebab Extravaganza",
+  "Embark on a culinary adventure with our kebab platter, featuring a delicious assortment of grilled lamb, chicken, and beef kebabs. Served with rice, salad, and a side of Turkish bread.",
+  kebabImage,
+  "kebab plate"
+);
+
+createCard(
+  "Lamb Skewer Sensation",
+  "Savor the tender and juicy lamb skewers in our Lamb Shish, grilled to perfection and bursting with flavor. A classic Turkish dish that is sure to please.",
+  lambShishImage,
+  "lamb shish"
+);
+
+createCard(
+  "Fiery Chicken Adana",
+  "Experience the fiery heat of our Chicken Adana, spicy minced chicken kebabs that are packed with flavor. A must-try for those who love a little spice.",
+  chickenAdanaImage,
+  "chicken Adana"
+);
