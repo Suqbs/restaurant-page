9351c8d0b30a56856b7ec766ad4ea6bd02dccb99
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-03 16:08:56 +0300

    Create logic for each section in the menus

--- src/menu-page.js
+++ src/menu-page.js
@@ -14,7 +14,7 @@ import chickenAdanaImage from "./image/chicken-adana.jpg";
 // image.src = kebabImage;
 // image.alt = "kebab plate";
 
-function createCard(title, intro, imgSrc, imgAlt) {
+function createCard(section, title, intro, imgSrc, imgAlt) {
   const card = document.createElement("div");
   card.classList.add("card");
 
@@ -33,26 +33,44 @@ function createCard(title, intro, imgSrc, imgAlt) {
   card.appendChild(cardTitle);
   card.appendChild(cardIntro);
   card.appendChild(image);
-
-  cardList.push(card);
+ 
+  section.push(card);
 }
 
-const cardList = [];
+function createSection(sectionTitle, sectionArray, content) {
 
-export function LoadMenuPage(content) {
-  for (let i = 0; i < cardList.length; i++) {
-    content.appendChild(cardList[i]);
+  const title = document.createElement("h1");
+  title.classList.add("section-title");
+  title.textContent = sectionTitle;
 
-    if (i !== cardList.length - 1) {
+  content.appendChild(title);
+
+  for (let i = 0; i < sectionArray.length; i++) {
+    content.appendChild(sectionArray[i]);
+
+    if (i !== sectionArray.length - 1) {
       const roundHr = document.createElement("hr");
       roundHr.classList.add("round-style");
 
       content.appendChild(roundHr);
     }
   }
+
 }
 
+export function LoadMenuPage(content) {
+
+    createSection("Main Dishes", mainDishes, content);
+
+}
+
+const mainDishes = [];
+const desserts = [];
+const beverages = [];
+
+/* Main Dishes */
 createCard(
+  mainDishes,
   "Kebab Extravaganza",
   "Embark on a culinary adventure with our kebab platter, featuring a delicious assortment of grilled lamb, chicken, and beef kebabs. Served with rice, salad, and a side of Turkish bread.",
   kebabImage,
@@ -60,6 +78,7 @@ createCard(
 );
 
 createCard(
+  mainDishes,
   "Lamb Skewer Sensation",
   "Savor the tender and juicy lamb skewers in our Lamb Shish, grilled to perfection and bursting with flavor. A classic Turkish dish that is sure to please.",
   lambShishImage,
@@ -67,6 +86,7 @@ createCard(
 );
 
 createCard(
+  mainDishes,
   "Fiery Chicken Adana",
   "Experience the fiery heat of our Chicken Adana, spicy minced chicken kebabs that are packed with flavor. A must-try for those who love a little spice.",
   chickenAdanaImage,
