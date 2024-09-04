3ef50f9e026e754011c7caebf11c0c0866b66440
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-04 23:43:45 +0300

    Trying to implement tab switch but not complete

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import { LoadAboutPage } from "./about-page";
 
 const content = document.getElementById("content");
 
+const tabs = document.querySelectorAll("#tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // Check if the content is already visible and avoid reloading
+  if (!tab.classList.contains("active")) {
+    // Remove active from all tabs first
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    // Then add active to the newly clicked one 
+    tab.classList.add("active");
+  }
+  });
+});
+
+const newContent = LoadHomePage(content);
 // LoadHomePage(content);
 // LoadMenuPage(content);
 LoadAboutPage(content);
