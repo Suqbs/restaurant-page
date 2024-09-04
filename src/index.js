b6ab6aff144c8a4a78c94ba4059a321dc6fd2085
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-04 23:52:18 +0300

    Change name page-load to initial-page-load again

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import "./styles.css";
+import { LoadInitialPage } from "./initial-page-load";
 import { LoadHomePage } from "./home-page";
 import { LoadMenuPage } from "./menu-page";
 import { LoadAboutPage } from "./about-page";
 
-const content = document.getElementById("content");
 
-const tabs = document.querySelectorAll("#tab");
+LoadContentInDemand();
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    // Check if the content is already visible and avoid reloading
-  if (!tab.classList.contains("active")) {
-    // Remove active from all tabs first
-    tabs.forEach(tab => tab.classList.remove('active'));
 
-    // Then add active to the newly clicked one 
-    tab.classList.add("active");
-  }
+function LoadContentInDemand() {
+  const content = document.getElementById("content");
+  const tabs = document.querySelectorAll(".tab");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      // Check if the content is already visible and avoid reloading
+      if (!tab.classList.contains("active")) {
+        // Remove active from all tabs first
+        tabs.forEach((tab) => tab.classList.remove("active"));
+
+        // Then add active to the newly clicked one
+        tab.classList.add("active");
+      }
+    });
   });
-});
+}
 
-const newContent = LoadHomePage(content);
 // LoadHomePage(content);
 // LoadMenuPage(content);
-LoadAboutPage(content);
+
