473b9bbcd3bf8d452b02ed716690801fa22e1ee7
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-05 14:25:15 +0300

    Add new content to home-page

--- src/home-page.js
+++ src/home-page.js
@@ -13,9 +13,37 @@ intro.classList.add("intro");
 intro.textContent =
   `Discover a culinary journey like no other at FURKAN RESTAURANT. Our passion for authentic Turkish cuisine shines through in every dish, crafted with the finest ingredients and served with a touch of warmth and hospitality. Whether you're seeking a casual meal or a special occasion, our menu offers a diverse range of options to satisfy every palate. From succulent kebabs to flavorful stews, each bite is a testament to our commitment to culinary excellence. Join us for a memorable dining experience at FURKAN RESTAURANT, where the flavors of Turkey come alive.`;
 
+const workingHoursTitle = document.createElement("h2");
+workingHoursTitle.classList.add("small-title");
+workingHoursTitle.textContent = "Working Hours";
+
+
+
+const weekDays = document.createElement("p");
+weekDays.textContent = "Weekdays: 1am - 1pm (CDT)";
+
+const weekends = document.createElement("p");
+weekends.textContent = "Weekends: 4am - 12pm (CDT)";
+
+const workingHours = document.createElement("div");
+workingHours.classList.add("working-hours");
+workingHours.appendChild(weekDays);
+workingHours.appendChild(weekends);
+
+const locationTitle = document.createElement("h2");
+locationTitle.classList.add("small-title");
+locationTitle.textContent = "Location";
+
+const location = document.createElement("p");
+location.textContent = "Chicago, Illinois(IL), 60638";
+
 export function LoadHomePage(content) {
     content.appendChild(title);
     content.appendChild(image);
     content.appendChild(intro);
+    content.appendChild(workingHoursTitle);
+    content.appendChild(workingHours);
+    content.appendChild(locationTitle);
+    content.appendChild(location);
 }
 
